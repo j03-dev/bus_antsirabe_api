100a1e877e67bd2f975b633e6478f074bfc7991c
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2025-11-06 18:32:14 +0300

    re struct the code

--- app/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from oxapy import serializer
-
-
-class TravelSerializer(serializer.Serializer):
-    primus = serializer.CharField()
-    terminus = serializer.CharField()
--- main.py
+++ main.py
@@ -1,12 +1,84 @@
-from oxapy import HttpServer, Cors  # type: ignore
-from app.core.app_state import AppState
-from app.api import bus
+from oxapy import HttpServer, Request, Cors, Status, Router, get, post, serializer
+from utils import load_bus_data, BusLine
 
-server = HttpServer(("0.0.0.0", 8080))
-server.app_data(AppState())
-server.attach(bus.router)
-server.cors(Cors())
+import typing
+
+
+class AppState:
+    def __init__(self):
+        self.buslines: typing.List[BusLine] = load_bus_data("data/travel.json")
+        self.travels = {
+            str(travel.id): travel.name
+            for busline in self.buslines
+            for travel in busline.travel
+        }
+        self.travel_sets = {
+            bus_line.id: {str(t.id) for t in bus_line.travel}
+            for bus_line in self.buslines
+        }
+
+        self.caches = {}
+
+
+class TravelSerializer(serializer.Serializer):
+    primus = serializer.CharField()
+    terminus = serializer.CharField()
+
+
+def get_travels(request: Request):
+    app_data: AppState = request.app_data
+    return app_data.travels
+
+
+def retrieve_travel(request: Request, id: str):
+    app_data: AppState = request.app_data
+    return {"travel": app_data.travels.get(id, None)} or Status.NOT_FOUND
+
+
+def find_bus(request: Request):
+    travel = TravelSerializer(request.data)
+    travel.is_valid()
+    primus = travel.validated_data["primus"]
+    terminus = travel.validated_data["terminus"]
+
+    app_data: AppState = request.app_data
+
+    bus_names = {
+        bus_line.name
+        for bus_line in app_data.buslines
+        if primus in app_data.travel_sets[bus_line.id]
+        and terminus in app_data.travel_sets[bus_line.id]
+    }
+
+    return list(bus_names)
+
+
+def cache(r: Request, next, **kwargs):
+    app_data: AppState = r.app_data
+    key = f"{r.method}/{r.uri}/{r.body}"
+    if response := app_data.caches.get(key, None):
+        return response
+    else:
+        response = next(r, **kwargs)
+        app_data.caches[key] = response
+        return response
+
+
+def main():
+    (
+        HttpServer(("0.0.0.0", 8080))
+        .cors(Cors())
+        .app_data(AppState())
+        .attach(
+            Router()
+            .middleware(cache)
+            .route(get("/api/travels", get_travels))
+            .route(get("/api/travels/{id}", retrieve_travel))
+            .route(post("/api/travels"), find_bus)
+        )
+        .run()
+    )
 
 
 if __name__ == "__main__":
-    server.run()
+    main()
