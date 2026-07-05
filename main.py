4933f679eee936ce5065aeacc48b30d8f7d741a4
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-07-05 11:18:56 +0300

    chore: bump version to 0.8.8

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import typing
 
 
 class AppState:
-    def __init__(self, bus_lines: typing.List[BusLine], travels: typing.List[Travel], travel_sets: dict) -> None:
+    def __init__(
+        self,
+        bus_lines: typing.List[BusLine],
+        travels: typing.List[Travel],
+        travel_sets: dict,
+    ) -> None:
         self.bus_lines = bus_lines
-        self.travels =travels
+        self.travels = travels
         self.travel_sets = travel_sets
         self.caches = {}
 
@@ -23,7 +28,7 @@ def cache_middleware(r: Request, next, **kwargs):
     primus = travel.validated_data["primus"]
     terminus = travel.validated_data["terminus"]
     key = f"{primus}/{terminus}"
-    app_data: AppState = r.app_data
+    app_data: AppState = r.app_data  # ty: ignore
     if response := app_data.caches.get(key, None):
         return response
     else:
@@ -34,7 +39,7 @@ def cache_middleware(r: Request, next, **kwargs):
         return response
 
 
-@get("/travels")
+@.get("/travels")
 def list_travels(r: Request):
     app_data: AppState = r.app_data
     if search := r.query.get("s"):
@@ -53,13 +58,13 @@ def search_bus(r: Request):
         bus_line
         for bus_line in app_data.bus_lines
         if r.primus in app_data.travel_sets[bus_line.id]
-           and r.terminus in app_data.travel_sets[bus_line.id]
+        and r.terminus in app_data.travel_sets[bus_line.id]
     ]
     return lines
 
 
 def main():
-    bus_lines : typing.List[BusLine] = load_bus_data("data/travel.json")
+    bus_lines: typing.List[BusLine] = load_bus_data("data/travel.json")
     travels = [t for busline in bus_lines for t in busline.travel]
     travel_sets = {bl.id: {t.id for t in bl.travel} for bl in bus_lines}
     app_data = AppState(bus_lines, travels, travel_sets)
